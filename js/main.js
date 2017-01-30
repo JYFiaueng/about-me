89a72ce1114a552f67cc555e795b0b25e6f6f1f3
jyuf <jyuf@foxmail.com>
2017-01-30 10:46:15 +0800

    完善

--- js/main.js
+++ js/main.js
@@ -1,49 +1,4 @@
 $(function (){
-    // 全屏插件设置
-    $('#dowebok').fullpage({
-        sectionsColor: ['#3498DB', '#2ECC71', '#eee', '#F39C12', '#FFFEBB'],
-        anchors: ['page1', 'page2', 'page3', 'page4', 'page5'],
-        continuousVertical: true,
-        menu: '#menu',
-        afterLoad:function(amchorLink, index){
-            if(amchorLink === 'page3'){
-                $('#SkillTree').css({
-                    height:$('body').height()-200
-                });
-                var frame = echarts.init(document.getElementById('SkillTree'));
-                frame.setOption(option);
-            }
-            if(amchorLink === 'page2'){
-                $('.headPicture').addClass('headPicture-active');
-                $('.whiteFont').addClass('whiteFont-active');
-            }
-            if(amchorLink === 'page4'){
-                $('.showImg img').css('opacity', '1');
-            }
-        },
-        afterRender:function (){
-            setTimeout(function (){
-                $('#mark').fadeOut(400);
-            }, 600);
-        },
-    });
-    // 根据可视区大小自动关闭滚动效果
-    $(window).resize(function(){
-        autoScrolling();
-    });
-    function autoScrolling(){
-        var $ww = $(window).width();
-        if($ww < 1024){
-            $.fn.fullpage.setAutoScrolling(false);
-        } else {
-            $.fn.fullpage.setAutoScrolling(true);
-        }
-    }
-    autoScrolling();
-    // 返回顶部
-    $("#backtop").click(function (){
-        $.fn.fullpage.moveTo('1');
-    });
     // echarts配置
     var option = {
         title: {
@@ -153,4 +108,73 @@ $(function (){
             }
         ]
     };
+    // 全屏插件设置
+    $('#dowebok').fullpage({
+        sectionsColor: ['#3498DB', '#2ECC71', '#eee', '#F39C12', '#FFFEBB'],
+        anchors: ['page1', 'page2', 'page3', 'page4', 'page5'],
+        continuousVertical: true,
+        menu: '#menu',
+        afterLoad:function(amchorLink, index){
+            if(amchorLink === 'page3'){
+                $('#SkillTree').css({
+                    height:$(window).height()-200
+                });
+                var frame = echarts.init(document.getElementById('SkillTree'));
+                frame.setOption(option);
+            }
+            if(amchorLink === 'page2'){
+                $('.headPicture').addClass('headPicture-active');
+                $('.whiteFont').addClass('whiteFont-active');
+            }
+            if(amchorLink === 'page4'){
+                $('.showImg img').css('opacity', '1');
+            }
+        },
+        afterRender:function (){
+            setTimeout(function (){
+                $('#mark').fadeOut(400);
+            }, 600);
+        },
+    });
+    // 根据可视区大小自动关闭滚动效果
+    var n = 0;
+    function throttle(method,context){//对函数进行节流
+        clearTimeout(method.tId);
+        method.tId=setTimeout(function(){
+            method.call(context);
+        },500);
+    }
+    $(window).resize(function(){
+        throttle(autoScrolling, window);
+    });
+    function autoScrolling(){
+        console.log('asd');
+        var $ww = $(window).width();
+        if($ww < 768){
+            $.fn.fullpage.setAutoScrolling(false);
+            option.radar[0].center = ['50%', '20%'];
+            option.radar[1].center = ['50%', '50%'];
+            option.radar[2].center = ['50%', '80%'];
+            option.radar[0].radius = 30;
+            option.radar[1].radius = 30;
+            option.radar[2].radius = 30;
+            var frame = echarts.init(document.getElementById('SkillTree'));
+            frame.setOption(option);
+        } else {
+            $.fn.fullpage.setAutoScrolling(true);
+            option.radar[0].center = ['20%', '60%'];
+            option.radar[1].center = ['50%', '40%'];
+            option.radar[2].center = ['80%', '60%'];
+            option.radar[0].radius = 100;
+            option.radar[1].radius = 100;
+            option.radar[2].radius = 100;
+            var frame = echarts.init(document.getElementById('SkillTree'));
+            frame.setOption(option);
+        }
+    }
+    autoScrolling();
+    // 返回顶部
+    $("#backtop").click(function (){
+        $.fn.fullpage.moveTo('1');
+    });
 });
